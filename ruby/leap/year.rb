4579dde1887afe6a411dc9e77ec3496952f2f9fa
James Harker <james@njtechnologies.co.uk>
2013-12-17 00:15:14 +0000

    Remove unnecessary methods in Ruby#Leap.

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -12,22 +12,10 @@ class Year
   private
 
   def regular_leap_year?
-    divisible_by_four? and !divisible_by_one_hundred?
+    divisible_by?(4) and !divisible_by?(100)
   end
 
   def exceptional_century?
-    divisible_by_four_hundred?
-  end
-
-  def divisible_by_four?
-    divisible_by?(4)
-  end
-
-  def divisible_by_one_hundred?
-    divisible_by?(100)
-  end
-
-  def divisible_by_four_hundred?
     divisible_by?(400)
   end
 
